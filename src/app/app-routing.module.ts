1c69e2f899d0439d6ead7c4b117658050cea9822
Aasimjazah <asimjazah8@gmail.com>
2023-04-11 01:22:24 +0530

    Added main page content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserSignInComponent } from './MyComponent/user-sign-in/user-sign-in.component';
 import { UserSignUpComponent } from './MyComponent/user-sign-up/user-sign-up.component';
 import { UserAreaComponent } from './MyComponent/user-area/user-area.component';
+import { MainComponent } from './MyComponent/main/main.component';
 
 const routes: Routes = [
   {path:"signIn" , component:UserSignInComponent},
   {path:"signUp" , component:UserSignUpComponent},
-  {path:"userArea", component: UserAreaComponent}
+  {path:"userArea", component: UserAreaComponent},
+  {path:"", component:MainComponent}
 ];
 
 @NgModule({
